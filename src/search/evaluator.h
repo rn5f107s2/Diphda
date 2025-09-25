650aa82c1d5cf3ce15165b6c786abf36cf26dd0f
rn5f107s2 <clemens.lerchl@gmail.com>
2025-09-25 11:42:59 +0200

    8x1 network ala leela

--- src/search/evaluator.h
+++ src/search/evaluator.h
@@ -103,7 +103,7 @@ public:
     Evaluator(int bs) : batchSize(bs), collector(Collector(bs)) {
         net = new Network(batchSize);
 
-        net->loadWeights("a0block.bin");
+        net->loadWeights("Leela8x1.bin");
 
         evaluationThread = std::thread(
             [&] {
