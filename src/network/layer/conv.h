b69495eeda4ad488891279d36726674fd0122cdf
rn5f107s2 <clemens.lerchl@gmail.com>
2025-09-25 20:48:55 +0200

    Retrun to dual net for default

--- src/network/layer/conv.h
+++ src/network/layer/conv.h
@@ -1,4 +1,4 @@
-#pragma onces
+#pragma once
 
 #include <cudnn.h>
 
--- src/search/evaluator.h
+++ src/search/evaluator.h
@@ -103,7 +103,7 @@ public:
     Evaluator(int bs) : batchSize(bs), collector(Collector(bs)) {
         net = new Network(batchSize);
 
-        net->loadWeights("Leela8x1.bin");
+        net->loadWeights("test256.bin");
 
         evaluationThread = std::thread(
             [&] {
