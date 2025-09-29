51ce0768c50611ddeb58058c835750da7d004e6e
rn5f107s2 <clemens.lerchl@gmail.com>
2025-09-29 15:25:59 +0200

    Backup

--- src/search/evaluator.h
+++ src/search/evaluator.h
@@ -103,7 +103,7 @@ public:
     Evaluator(int bs) : batchSize(bs), collector(Collector(bs)) {
         net = new Network(batchSize);
 
-        net->loadWeights("zeroRLNet10.bin");
+        net->loadWeights("zeroRLNet13.bin");
 
         evaluationThread = std::thread(
             [&] {
